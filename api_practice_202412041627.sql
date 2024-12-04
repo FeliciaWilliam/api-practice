47e8eb38c8cb8b1856fb8c97fb2fca00bba56316
FeliciaWilliam <84226481+FeliciaWilliam@users.noreply.github.com>
2024-12-04 22:34:57 +0700

    Add files via upload

---
+++ api_practice_202412041627.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.api_practice (id,title,description,status,priority,due_date,created_at,updated_at) VALUES
+	 (1,'First Task','This is the first task description.','open','high','2024-12-10','2024-12-02','2024-12-02');
